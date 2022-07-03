5c00e66c0805f4b8ae3666a76d0d806df169bf35
Swift-dot <lawalquadri507@gmail.com>
2022-07-03 17:35:30 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Header from './Header';
-import Content from './Content';
 import Footer from './Footer';
 import Count from './Count_Down/Count';
 
